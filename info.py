75efa82e0ebd7871e966b175cbadb5f4ca54316a
Cinemahubbots <126318209+Cinemahubbots@users.noreply.github.com>
2024-01-24 18:07:02 +0530

    Update info.py

--- info.py
+++ info.py
@@ -22,7 +22,7 @@ USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', False))
 PICS = (environ.get('PICS', 'https://telegra.ph/file/00aade75a294d7b8a0c0c.jpg https://telegra.ph/file/a7a87d8dd0c321e4b6728.jpg https://telegra.ph/file/892e81f85f4e809d5b542.jpg https://telegra.ph/file/9534520f6e89103ab03c2.jpg')).split()
 
 # Admins, Channels & Users
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '5652656279 1008683516 1419727822').split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '6480034504 5531461861 1008683516 1419727822').split()]
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1001909094733').split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
